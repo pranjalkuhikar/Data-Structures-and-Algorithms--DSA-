07330a83782a674d25479e31ecf12a6936b5f9d3
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 20:03:25 +0530

    remove commented-out GCD/LCM code and retain Armstrong number logic

--- Step1/Basic_Maths/index.js
+++ Step1/Basic_Maths/index.js
@@ -26,16 +26,32 @@
 // }
 // console.log(temp === rev ? "Palindrome" : "Not Palindrome");
 
-let n1 = 9;
-let n2 = 12;
-let temp1 = n1;
-let temp2 = n2;
-while (n2 != 0) {
-  let temp = n2;
-  n2 = n1 % n2;
-  n1 = temp;
+// let n1 = 9;
+// let n2 = 12;
+// let temp1 = n1;
+// let temp2 = n2;
+// while (n2 != 0) {
+//   let temp = n2;
+//   n2 = n1 % n2;
+//   n1 = temp;
+// }
+// let GCD = n1;
+// console.log(GCD);
+// let LCM = (temp1 * temp2) / GCD;
+// console.log(LCM);
+
+let n = 1634;
+let copy = n;
+let temp = n;
+let count = 0;
+let sum = 0;
+while (n != 0) {
+  count++;
+  n = Math.floor(n / 10);
 }
-let GCD = n1;
-console.log(GCD);
-let LCM = (temp1 * temp2) / GCD;
-console.log(LCM);
+while (temp != 0) {
+  let lastDigit = temp % 10;
+  sum += lastDigit ** count;
+  temp = Math.floor(temp / 10);
+}
+console.log(sum === copy ? "Armstrong Number" : "Not Armstrong Number");
