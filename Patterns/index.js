86c1be8db73ff779fdbc77b0adc51d3e27860991
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 18:43:12 +0530

    refactor pattern printing to output a hollow square pattern with numbered layers

--- Patterns/index.js
+++ Patterns/index.js
@@ -230,9 +230,11 @@ let n = 5;
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
     if (i === 0 || i === n - 1 || j === 0 || j === n - 1) {
-      process.stdout.write("*");
+      process.stdout.write("3 ");
+    } else if (i === 1 || i === n - 2 || j === 1 || j === n - 2) {
+      process.stdout.write("2 ");
     } else {
-      process.stdout.write(" ");
+      process.stdout.write("1 ");
     }
   }
   console.log();
