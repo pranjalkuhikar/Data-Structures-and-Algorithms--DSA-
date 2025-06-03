283cd9a3ea0928cfb91da3f178b4f882a04398b0
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 21:30:33 +0530

    refactor: comment out previous fibonacci implementation and update to loop for better clarity

--- Step1/Basic_Recursion/index.js
+++ Step1/Basic_Recursion/index.js
@@ -53,9 +53,18 @@
 // }
 // console.log(checkPalindrome(0, str) ? "Palindrome" : "Not a Palindrome");
 
+// let n = 6;
+// function fibonacci(n) {
+//   if (n === 0 || n == 1) return n;
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+// console.log(fibonacci(n));
+
 let n = 6;
 function fibonacci(n) {
   if (n === 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
-console.log(fibonacci(n));
+for (let i = 0; i < n; i++) {
+  console.log(fibonacci(i));
+}
