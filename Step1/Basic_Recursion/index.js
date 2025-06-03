731423d0343c1a44cdc660f522d48ffb9573bef3
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 20:53:32 +0530

    refactor: update printNumber function to log numbers after recursive call

--- Step1/Basic_Recursion/index.js
+++ Step1/Basic_Recursion/index.js
@@ -6,10 +6,18 @@
 // }
 // printName(n);
 
+// let n = 5;
+// function printNumber(n) {
+//   if (n === 0) return;
+//   console.log(n);
+//   printNumber(n - 1);
+// }
+// printNumber(n);
+
 let n = 5;
 function printNumber(n) {
   if (n === 0) return;
-  console.log(n);
   printNumber(n - 1);
+  console.log(n);
 }
 printNumber(n);
