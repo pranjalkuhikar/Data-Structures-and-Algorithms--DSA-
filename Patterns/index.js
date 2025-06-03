d361a26326f17ed21c54b1b7a4f51972dcdabc74
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 17:00:13 +0530

    create a right-angled triangle

--- Patterns/index.js
+++ Patterns/index.js
@@ -1,6 +1,14 @@
 let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < n; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
 for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
+  for (let j = 0; j <= i; j++) {
     process.stdout.write("*");
   }
   console.log();
