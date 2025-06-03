482bb527426f533aa3a6a3e61dfc8c1c048c614f
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 17:03:51 +0530

    replace asterisks with incremental numbers in pattern printing

--- Patterns/index.js
+++ Patterns/index.js
@@ -7,9 +7,17 @@ let n = 5;
 //   console.log();
 // }
 
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
 for (let i = 0; i < n; i++) {
+  let count = 1;
   for (let j = 0; j <= i; j++) {
-    process.stdout.write("*");
+    process.stdout.write(`${count++}`);
   }
   console.log();
 }
