0c97fa9a6460a8d83f40bd24cabdbe150863dcd4
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 19:59:45 +0530

    remove commented-out palindrome check code and retain GCD/LCM calculations

--- Step1/Basic_Maths/index.js
+++ Step1/Basic_Maths/index.js
@@ -16,12 +16,26 @@
 // }
 // console.log(rev);
 
-let n = 4554;
-let rev = 0;
-let temp = n;
-while (n != 0) {
-  let lastDigit = n % 10;
-  rev = rev * 10 + lastDigit;
-  n = Math.floor(n / 10);
+// let n = 4554;
+// let rev = 0;
+// let temp = n;
+// while (n != 0) {
+//   let lastDigit = n % 10;
+//   rev = rev * 10 + lastDigit;
+//   n = Math.floor(n / 10);
+// }
+// console.log(temp === rev ? "Palindrome" : "Not Palindrome");
+
+let n1 = 9;
+let n2 = 12;
+let temp1 = n1;
+let temp2 = n2;
+while (n2 != 0) {
+  let temp = n2;
+  n2 = n1 % n2;
+  n1 = temp;
 }
-console.log(temp === rev ? "Palindrome" : "Not Palindrome");
+let GCD = n1;
+console.log(GCD);
+let LCM = (temp1 * temp2) / GCD;
+console.log(LCM);
