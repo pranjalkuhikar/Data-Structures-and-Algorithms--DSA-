270c79c51606a2248202915f12d9abce3ea71f3e
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 19:52:39 +0530

    refactor palindrome check logic and clean up commented code

--- Step1/Basic_Maths/index.js
+++ Step1/Basic_Maths/index.js
@@ -7,11 +7,21 @@
 // }
 // console.log(count);
 
-let n = 12345;
+// let n = 12345;
+// let rev = 0;
+// while (n != 0) {
+//   let lastDigit = n % 10;
+//   rev = rev * 10 + lastDigit;
+//   n = Math.floor(n / 10);
+// }
+// console.log(rev);
+
+let n = 4554;
 let rev = 0;
+let temp = n;
 while (n != 0) {
   let lastDigit = n % 10;
   rev = rev * 10 + lastDigit;
   n = Math.floor(n / 10);
 }
-console.log(rev);
+console.log(temp === rev ? "Palindrome" : "Not Palindrome");
