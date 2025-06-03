0d2b6d324cfa0f727a4414d447f2c76c9aaf48f8
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 17:23:16 +0530

    refactor pattern printing to add a new pyramid structure and adjust spacing for alignment

--- Patterns/index.js
+++ Patterns/index.js
@@ -56,8 +56,27 @@ let n = 5;
 //   console.log();
 // }
 
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     process.stdout.write(" ");
+//   }
+//   for (let k = 2 * i; k <= 2 * n - 2; k++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
 for (let i = 0; i < n; i++) {
-  for (let j = 0; j < i; j++) {
+  for (let j = i; j < n; j++) {
+    process.stdout.write(" ");
+  }
+  for (let k = 0; k <= 2 * i; k++) {
+    process.stdout.write("*");
+  }
+  console.log();
+}
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j <= i; j++) {
     process.stdout.write(" ");
   }
   for (let k = 2 * i; k <= 2 * n - 2; k++) {
