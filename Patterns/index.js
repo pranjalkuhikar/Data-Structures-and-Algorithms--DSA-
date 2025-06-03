2bfb5cac319d107e5dcfe2455caf2366fb7b2547
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-03 17:46:51 +0530

    refactor pattern printing to implement a new counting pattern with spacing

--- Patterns/index.js
+++ Patterns/index.js
@@ -107,17 +107,25 @@ let n = 5;
 //   console.log();
 // }
 
-for (let i = 1; i < n; i++) {
-  let count = 1;
-  for (let j = 1; j <= i; j++) {
-    process.stdout.write(`${count++}`);
-  }
-  for (let k = 2; k < 2 * (n - i); k++) {
-    process.stdout.write(" ");
-  }
-  count = i;
-  for (let j = 1; j <= i; j++) {
-    process.stdout.write(`${count--}`);
+// for (let i = 1; i < n; i++) {
+//   let count = 1;
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(`${count++}`);
+//   }
+//   for (let k = 2; k < 2 * (n - i); k++) {
+//     process.stdout.write(" ");
+//   }
+//   count = i;
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(`${count--}`);
+//   }
+//   console.log();
+// }
+
+let count = 1;
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j <= i; j++) {
+    process.stdout.write(`${count++} `);
   }
   console.log();
 }
