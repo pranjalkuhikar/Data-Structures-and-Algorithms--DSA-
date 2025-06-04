9776e5266ee9f0361c400956a464f4d3033dd616
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-04 18:45:53 +0530

    bubble sort implementation

--- Step2/Sorting-1/index.js
+++ Step2/Sorting-1/index.js
@@ -1,9 +1,19 @@
+// let arr = [13, 46, 24, 52, 20, 9];
+// for (let i = 0; i < arr.length - 1; i++) {
+//   let min = i;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[j] < arr[min]) min = j;
+//     [arr[min], arr[i]] = [arr[i], arr[min]];
+//   }
+// }
+// console.log(arr);
+
 let arr = [13, 46, 24, 52, 20, 9];
-for (let i = 0; i < arr.length - 1; i++) {
-  let min = i;
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[j] < arr[min]) min = j;
-    [arr[min], arr[i]] = [arr[i], arr[min]];
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
   }
 }
 console.log(arr);
