51665818ba549887358f7db7282067f8285e2359
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-05 20:17:52 +0530

    refactor: comment out linear search implementation and retain binary search

--- Step2/Searching/index.js
+++ Step2/Searching/index.js
@@ -1,12 +1,32 @@
-function linearSearch(arr, k) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === k) {
-      return i;
+// function linearSearch(arr, k) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === k) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// let arr = [13, 46, 24, 52, 20, 9];
+// let k = 52;
+// console.log(linearSearch(arr, k));
+
+function binarySearch(arr, k) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === k) {
+      return mid;
+    } else if (arr[mid] < k) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
   return -1;
 }
 
-let arr = [13, 46, 24, 52, 20, 9];
-let k = 52;
-console.log(linearSearch(arr, k));
+let arr = [1, 2, 3, 4, 5, 6];
+let k = 2;
+console.log(binarySearch(arr, k));
