dfe5d6578e28629e647887916b5f863029d2a99c
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-06 13:58:32 +0530

    refactor: add time and space complexity comments for array functions and comment out removeDuplicate implementations

--- Step3/Easy/index.js
+++ Step3/Easy/index.js
@@ -1,4 +1,6 @@
 // Question 1: Find the maximum element in an array
+// Time Complexity: O(n) - single pass through array
+// Space Complexity: O(1) - only using one variable
 // function findLargest(arr) {
 //   let max = 0;
 //   for (let i = 0; i < arr.length; i++) {
@@ -12,6 +14,8 @@
 // console.log(findLargest(arr));
 
 // Question 2: Find the second maximum element in an array
+// Time Complexity: O(n) - single pass through array
+// Space Complexity: O(1) - using two variables
 // function findSecondLargest(arr) {
 //   let max = arr[0];
 //   let sMax = arr[0];
@@ -29,6 +33,8 @@
 // console.log(findSecondLargest(arr));
 
 // Question 3: Check if an array is sorted
+// Time Complexity: O(n) - single pass through array
+// Space Complexity: O(1) - using only boolean variable
 // function sortedArray(arr) {
 //   let isSorted = true;
 //   for (let i = 0; i < arr.length; i++) {
@@ -43,24 +49,30 @@
 // console.log(sortedArray(arr));
 
 // Question 4: Remove duplicates from an array
-function removeDuplicateSet(arr) {
-  let set = new Set(arr);
-  return set;
-}
-function removeDuplicate(arr) {
-  let i = 0;
-  for (let j = 1; j < arr.length; j++) {
-    if (arr[i] != arr[j]) {
-      i++;
-      arr[i] = arr[j];
-    }
-  }
-  return i + 1;
-}
-let arr = [1, 1, 2, 2, 3, 3, 4, 5, 5];
-console.log([...removeDuplicateSet(arr)]);
-// If when array is sorted
-let n = removeDuplicate(arr);
-for (let i = 0; i < n; i++) {
-  console.log(arr[i]);
-}
+// Method 1: Using Set
+// Time Complexity: O(n) - creating Set from array
+// Space Complexity: O(n) - storing unique elements in Set
+// function removeDuplicateSet(arr) {
+//   let set = new Set(arr);
+//   return set;
+// }
+// Method 2: Two Pointer approach (for sorted array)
+// Time Complexity: O(n) - single pass through array
+// Space Complexity: O(1) - in-place modification
+// function removeDuplicate(arr) {
+//   let i = 0;
+//   for (let j = 1; j < arr.length; j++) {
+//     if (arr[i] != arr[j]) {
+//       i++;
+//       arr[i] = arr[j];
+//     }
+//   }
+//   return i + 1;
+// }
+// let arr = [1, 1, 2, 2, 3, 3, 4, 5, 5];
+// console.log([...removeDuplicateSet(arr)]);
+// // If when array is sorted
+// let n = removeDuplicate(arr);
+// for (let i = 0; i < n; i++) {
+//   console.log(arr[i]);
+// }
