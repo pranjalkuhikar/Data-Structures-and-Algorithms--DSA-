46bec2ce481897cbc4c963c4b11b8402017f8379
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-09 22:05:57 +0530

    Left Rotation

--- Step3/Easy/index.js
+++ Step3/Easy/index.js
@@ -76,3 +76,12 @@
 // for (let i = 0; i < n; i++) {
 //   console.log(arr[i]);
 // }
+
+// Question 5: Left Rotate the Array by One
+let arr = [1, 2, 3, 4, 5];
+let temp = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = arr[i + 1];
+}
+arr[arr.length - 1] = temp;
+console.log(arr);
